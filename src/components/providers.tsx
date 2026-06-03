aff4713bbfba152e0ce4636f9ded5ce3c3aef06a
santleonardo <54089176+santleonardo@users.noreply.github.com>
2026-06-03 18:14:24 -0300

    Delete src/components directory

--- src/components/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import React from 'react'
-import { AuthProvider } from '@/hooks/use-auth'
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>
-}
